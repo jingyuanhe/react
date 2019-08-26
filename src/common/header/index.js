4b4e1847cadfa201bf4f5712f0461284e9a372bb
jingyuanhe <389026847@qq.com>
2019-08-26 21:15:59 +0800

    immutable

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -41,7 +41,7 @@ const Header=(props)=>{
 }
 const mapStateToProps=(state)=>{
     return{
-        focused:state.header.focused
+        focused:state.get('header').get('focused')
     }
 }
 const mapDispathToProps=(dispatch)=>{
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,17 +1,14 @@
 import * as actionTypes from './actionTypes'
-const defaultState={
+import {fromJS} from 'immutable'
+const defaultState=fromJS({
     focused:false
-}
+})
 export default (state=defaultState,action)=>{
     switch(action.type){
         case actionTypes.SEARCH_FOCUS:
-            return{
-                focused:true
-            }
+            return state.set('focused',true)
         case actionTypes.SEARCH_BLUR:
-            return{
-                focused:false
-            }
+            return state.set('focused',false)
          default:
              return state       
     }
